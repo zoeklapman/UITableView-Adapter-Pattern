17814f836eb0b3b13a2197a0622ff2eb2ffff483
Zoë <zoeklapman@gmail.com>
2023-06-24 18:38:32 -0700

    Initial Commit

---
+++ AdapterTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdapterTableView
+//
+//  Created by Zoë Klapman on 6/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
